fac82fe0b937d4e76007340ede22b4575a8ecc97
rapilap <raffyabdillah03@gmail.com>
2024-12-15 13:55:03 +0700

    add tailwind

---
+++ resources/views/percobaan.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite ('resources/css/app.css')
+    <title>Coba</title>
+</head>
+<body>
+    <div class="px-4 py-4 bg-gray-500 text-white">Hello, World!</div>
+</body>
+</html>
