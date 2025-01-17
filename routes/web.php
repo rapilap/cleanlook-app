e0930f57a0024e30b423548053fecadf9fa34ea1
rapilap <raffyabdillah03@gmail.com>
2025-01-17 23:03:54 +0700

    preparing for landfill location

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AccountListController;
+use App\Http\Controllers\LandfillController;
 use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 
@@ -23,6 +24,8 @@ Route::group([
     Route::get('/income', function() {
         return view('admin.income');
     });
+
+    Route::get('/location', [LandfillController::class, 'index'])->name('landfill.index');
 
     Route::get('/dashboard', function () {
         return view('admin.dashboard');
