25c031009788bcc5512c999628a8cb5c72be3adb
rapilap <raffyabdillah03@gmail.com>
2025-01-26 09:25:34 +0700

    preparing for payment w/ midtrans

---
+++ config/midtrans.php
@@ -0,0 +1,6 @@
+<?php
+    return [
+        'merchantID' => env('MIDTRANS_MERCHANT_ID'),
+        'clientKey' => env('MIDTRANS_CLIENT_KEY'),
+        'serverKey' => env('MIDTRANS_SERVER_KEY'),
+    ];
