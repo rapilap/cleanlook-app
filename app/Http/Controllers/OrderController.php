c1f8fa802be691f41e6f627898daea649a61f29f
rapilap <raffyabdillah03@gmail.com>
2025-01-27 14:48:04 +0700

    fix insert transaction data request

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Transaction;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -10,17 +11,22 @@ class OrderController extends Controller
 {
     public function index()
     {
-        return view('berandauser');
+        $category = Category::all();
+        
+        return view('berandauser',compact('category'));
     }
 
+    
     public function payment(Request $request)
     {
+        
         $request->request->add(['price'=>$request->price, 'status' => 'unpaid']);
         $transaction = Transaction::create([
             'user_id' => Auth::id(),
             'courier_id' => null, // Belum ada kurir
             'category_id' => $request->type_sampah,
             'landfill_id' => $request->landfill,
+            'date'=>now(),
             'pickup_lat' => $request->pickup_lat,
             'pickup_long' => $request->pickup_long,
             'address' => $request->alamat,
@@ -29,6 +35,8 @@ class OrderController extends Controller
             'status' => 'unpaid',
         ]);
 
+        $user = Auth::user();
+        
         // dd($transaction);
         // Set your Merchant Server Key
         \Midtrans\Config::$serverKey = config('midtrans.serverKey');
@@ -45,13 +53,31 @@ class OrderController extends Controller
                 'gross_amount' => $transaction->price,
             ),
             'customer_details' => array(
-                'first_name' => $request->name,
+                'first_name' => $user->name,
                 'last_name' => '',
-                'phone' => $request->phone,
+                'phone' => $user->phone,
             ),
         );
-
+        
         $snapToken = \Midtrans\Snap::getSnapToken($params);
         return view('berandauser', compact('snapToken', 'transaction'));
+    }
+    
+    public function calculateTotal($category_id, $weight, $distance)
+    {
+        // Ambil kategori dari database
+        $category = Category::find($category_id);
+    
+        if (!$category) {
+            return response()->json(['error' => 'Category not found'], 404);
+        }
+    
+        // Hitung total harga
+        $categoryPrice = $category->cat_price; // Harga per kg
+        $distanceCost = $distance * 5000; // Biaya per km
+        $weightCost = $weight * $categoryPrice; // Biaya per kg
+        $totalPrice = ceil($distanceCost + $weightCost); // Total harga
+    
+        return $totalPrice;
     }
 }
