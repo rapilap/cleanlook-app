c93b0ef8e359182e5c8aae738788def9ba662c31
rapilap <raffyabdillah03@gmail.com>
2025-01-29 22:29:25 +0700

    fix payment

--- config/midtrans.php
+++ config/midtrans.php
@@ -3,4 +3,5 @@
         'merchantID' => env('MIDTRANS_MERCHANT_ID'),
         'clientKey' => env('MIDTRANS_CLIENT_KEY'),
         'serverKey' => env('MIDTRANS_SERVER_KEY'),
+        'isProduction' => env('MIDTRANS_IS_PRODUCTION_KEY')
     ];
