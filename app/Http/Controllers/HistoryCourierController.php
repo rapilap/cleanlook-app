8fa8be9fe5d101986afbf879f4feaca7e343a28f
rapilap <raffyabdillah03@gmail.com>
2025-02-03 22:42:26 +0700

    edit id sort

--- app/Http/Controllers/HistoryCourierController.php
+++ app/Http/Controllers/HistoryCourierController.php
@@ -10,37 +10,32 @@ use Illuminate\Support\Facades\Auth;
 
 class HistoryCourierController extends Controller
 {
-    // Mendapatkan semua transaksi
+    
     public function index()
-{
-    $user = Auth::guard('courier')->user();
-    $id_courier = $user->id;
+    {
+        $user = Auth::guard('courier')->user();
+        $id_courier = $user->id;
 
-    // Ambil tanggal hari ini
-    $today = Carbon::today();
+        $today = Carbon::today();
 
-    // Ambil riwayat transaksi untuk kurir berdasarkan hari ini
-    $history_courier = Transaction::with('courier')
-        ->where('courier_id', $id_courier)
-        ->orderBy('id', 'asc')
-        ->get();
+        $history_courier = Transaction::with('courier')
+            ->where('courier_id', $id_courier)
+            ->orderBy('id', 'desc')
+            ->get();
 
-    // Ambil total pendapatan saat ini berdasarkan transaksi hari ini
-    $totalPendapatan = Transaction::where('courier_id', $id_courier)
-        ->whereDate('date', $today)
-        ->sum('price'); // Asumsi `price` adalah total pembayaran dari transaksi
+        $totalPendapatan = Transaction::where('courier_id', $id_courier)
+            ->whereDate('date', $today)
+            ->sum('price');
 
-    // Ambil pemasukan terakhir dari transaksi terakhir hari ini
-    $lastTransaction = Transaction::where('courier_id', $id_courier)
-    ->whereDate('created_at', $today)
-    ->latest('created_at')
-    ->first();
+        $lastTransaction = Transaction::where('courier_id', $id_courier)
+        ->whereDate('created_at', $today)
+        ->latest('created_at')
+        ->first();
 
-    // Ambil harga transaksi terakhir
-    $lastTransactionPrice = $lastTransaction ? $lastTransaction->price : 0;
+        $lastTransactionPrice = $lastTransaction ? $lastTransaction->price : 0;
 
-    return view('courier.pendapatan', compact('history_courier', 'user', 'today', 'lastTransactionPrice', 'totalPendapatan'));
-}
+        return view('courier.pendapatan', compact('history_courier', 'user', 'today', 'lastTransactionPrice', 'totalPendapatan'));
+    }
 
 }
 
